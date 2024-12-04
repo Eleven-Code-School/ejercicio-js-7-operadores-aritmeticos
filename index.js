f51bccc92630d0dcc1c6527c998611b238310832
Fernando Herrero <herrerofernando.94@gmail.com>
2024-12-04 23:47:00 +0100

    add till exercise 7 done

--- index.js
+++ index.js
@@ -2,18 +2,82 @@
 
 // Ejercicio 1: Suma de productos
 
+let precioProducto1 = 34;
+
+let precioProducto2 = 25;
+
+const precioTotal = precioProducto1 + precioProducto2;
+
+console.log("El precio total es de", precioTotal);
+
+if(precioTotal >= 100) {
+    console.log("Que lo compre su prima")
+} else {;
+    console.log("Me lo puedo permitir")
+}
 // Ejercicio 2: Descuento en una compra
 
+let precioOriginal = 26;
+
+let descuento = 5;
+
+const precioFinal = precioOriginal - descuento;
+
+console.log("El producto queda rebajado a", precioFinal);
 // Ejercicio 3: Calcula el IVA
 
+let precio = 73;
+let iva = 21;
+
+// Cálculo del precio total incluyendo el IVA
+const precioTotalConIva = precio * (1 + iva / 100);
+
+console.log("El precio total con el iva es del", precioTotalConIva);
 // Ejercicio 4: División de la cuenta
 
+let totalCuenta = 134;
+
+let numPersonas = 3;
+
+const divisionDeLaCuenta = totalCuenta / numPersonas;
+
+console.log("Cada uno toca a", divisionDeLaCuenta);
 // Ejercicio 5: Resto de una división
+
+let totalChocolates = 38;
+
+let personas = 5;
+
+const chocolatesQueSobran = totalChocolates % personas;
+
+console.log("Sobran la cantidad de", chocolatesQueSobran, "chocolates");
 
 // Ejercicio 6: ¿Es un número par?
 
+const numero = 44;
+
+console.log("El número", numero);
+
+if(numero % 2 === 0) {
+    console.log("Es par")
+} else {
+    console.log("Es impar")
+};
 // Ejercicio 7: Multiplica ingredientes
 
+const cantidadOriginal = 345; 
+
+let factorMultiplicacion = 2;
+
+let cantidadFinal = cantidadOriginal * factorMultiplicacion;
+
+console.log("La cantidad de ingredientes si los duplico es de", cantidadFinal);
+
+factorMultiplicacion = 3;
+
+cantidadFinal = cantidadOriginal * factorMultiplicacion;
+
+console.log("La cantidad de los ingredientes ahora es", cantidadFinal);
 // Ejercicio 8: Incremento en la cuenta
 
 // Ejercicio 9: Decremento de una deuda
