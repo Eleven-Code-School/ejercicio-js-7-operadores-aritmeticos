d1673cd39249caf303dc1c914c1510fdbcff48eb
Eduard Mosquera <edumarim@hotmail.com>
2024-12-09 16:47:36 +0100

    completed up to exercise num 27

--- index.js
+++ index.js
@@ -1,35 +1,33 @@
 // Ejercicios de Operadores Aritméticos
 
 // Ejercicio 1: Suma de productos
+
 let precioProducto1 = 5 ;
 let precioProducto2 = 2 ;
-
 let resultado = (precioProducto1 +  precioProducto2);
 
-console.log (resultado)
-
+console.log (resultado);
 
 // Ejercicio 2: Descuento en una compra
 
 let precioOriginal = 24;
 let descuento = 2 ;
-
 let descuentoAplicado= ( precioOriginal - descuento);
+
 console.log ("Juan el coche con descuento queda en  " + descuentoAplicado)
+
 // Ejercicio 3: Calcula el IVA
 
 let precio = 340;
 let iva = 0.21 ;
-
 let precioTotal = precio + (precio * iva);
 
-console.log ("El precio total es: " + precioTotal)
+console.log ("El precio total es: " + precioTotal);
 
 // Ejercicio 4: División de la cuenta
 
 let totalCuenta = 60 ;
 let numPersonas = 2;
-
 let cuentaDividida = ( totalCuenta / numPersonas);
 
 console.log ( "a cada persona le toca de " + cuentaDividida );
@@ -38,7 +36,6 @@ console.log ( "a cada persona le toca de " + cuentaDividida );
 
 let totalChocolates = 678 ;
 let personas = 50 ;
-
 let resultadoDivision = ( totalChocolates % personas);
 
 console.log ( "el restante al repartir los chocolates es " + resultadoDivision)
@@ -47,7 +44,6 @@ console.log ( "el restante al repartir los chocolates es " + resultadoDivision)
 // Ejercicio 6: ¿Es un número par?
 
 let numero = 2 ;
-
 if (numero % 2 === 0) {
     console.log ( numero + " es par")
 } else {
@@ -57,73 +53,167 @@ if (numero % 2 === 0) {
 // Ejercicio 7: Multiplica ingredientes
 
 let cantidadOriginal = 20 ;
-
 let factorMultiplicacion = 2;
-
 let cantidadTotal = cantidadOriginal * factorMultiplicacion ;
 
-console.log ( cantidadTotal)
+console.log ( cantidadTotal);
 
 
 // Ejercicio 8: Incremento en la cuenta
 
 let totalAhorros = 52;
-
 aumentoDiez = totalAhorros += 10 ;
 
-console.log ("el total de ahorros con un aumento de 10 es "+aumentoDiez)
+console.log ("el total de ahorros con un aumento de 10 es "+aumentoDiez);
 
 // Ejercicio 9: Decremento de una deuda
 
-let deuda = 50
-
+let deuda = 50 ;
 menosQuince = deuda -= 15 ;
+
 console.log("su deuda agregando un -15 es " + menosQuince);
 
 // Ejercicio 10: Calcula un exponente
 
 let base = 56 ;
 let exponente = 3;
-
-totalExponente = base ** exponente;
+let totalExponente = base ** exponente;
 
 console.log ( "la base " + base + " elevado a " + exponente + " es " + totalExponente)
 
 // Ejercicio 11: Reducción de precios
 
+let precioProducto = 56;
+let DescuentoDiez = precioProducto * 0.9; 
+
+console.log("El producto cuesta " + precioProducto + " y con el descuento del 10% queda en " + DescuentoDiez);
+
 // Ejercicio 12: Resto acumulado
+
+let cantidad = 1786736;
+cantidad %= 3;
+
+console.log( cantidad);
 
 // Ejercicio 13: Incremento antes del uso
 
-// Ejercicio 14: Decremento después del uso
+let dias = 5;
+++dias;
+
+console.log (dias);
+
+// Ejercicio 14: Decremento después del uso 
+
+let cantidades = 90;
+--cantidades ;
+
+console.log (cantidades);
 
 // Ejercicio 15: Ajuste del salario
 
+let salario = 1500;
+salario += salario * 0.05;
+
+console.log (salario);
+
 // Ejercicio 16: Calcula el área de un rectángulo
+
+let baseAncho = 189;
+let altura = 400;
+let totalArea = baseAncho * altura;
+
+console.log (totalArea)
 
 // Ejercicio 17: Divide un premio
 
+let premioTotal = 690;
+let numGanadores = 45;
+let GananciaIndividual = premioTotal / numGanadores;
+
+console.log (GananciaIndividual);
+
 // Ejercicio 18: Incremento en el inventario
+
+let inventario = 200;
+inventario += 50;
+
+console.log(inventario);
 
 // Ejercicio 19: Gasto semanal
 
+let gastoDiario = 20 ;
+let gastoSemanal = gastoDiario * 7;
+
+console.log ( gastoSemanal);
+
+
 // Ejercicio 20: Conversión de monedas
+
+let cantidadUSD = 30;
+let tasaCambio= 4000;
+let valorPesos= cantidadUSD * tasaCambio ;
+
+console.log (valorPesos);
 
 // Ejercicio 21: Cálculo de descuentos acumulados
 
+let precioInicial = 5600;
+precioInicial *= 0.8;
+precioInicial *= 0.9;
+
+console.log ( precioInicial);
+
 // Ejercicio 22: ¿Qué edad tendré en 5 años?
+
+let edadActual = 33;
+edadActual += 5;
+
+console.log( "en 5 años tendria " + edadActual + " años")
+
 
 // Ejercicio 23: Cuota mensual
 
+let deudaTotal =  50000;
+let meses = 72;
+let cuotas = deudaTotal / meses;
+
+console.log ( cuotas )
+
+
 // Ejercicio 24: Incremento antes de imprimir
+
+let puntos = 83;
+++puntos;
+
+console.log(puntos)
 
 // Ejercicio 25: Decremento después de imprimir
 
+let turnos = 38;
+--turnos;
+
+console.log(turnos)
+
 // Ejercicio 26: Comprueba divisibilidad por 5
+
+let number = 789;
+if (number % 5 === 0){
+    console.log(number + " es divisible por 5" );
+}else {
+    console.log("no es divisible por 5");
+}
 
 // Ejercicio 27: Saldo restante después de un retiro
 
+let saldo = 974;
+let retiro =298;
+let nuevoSaldo= saldo-retiro;
+
+console.log ( "su nuevo saldo es: "+ nuevoSaldo);
+
 // Ejercicio 28: Cálculo del perímetro de un cuadrado
+
+let lado = 35;
 
 // Ejercicio 29: Conversión de días
 
